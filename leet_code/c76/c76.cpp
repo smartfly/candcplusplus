269d186b9df2a869ad60127ffd6a95bb8f70a353
taohu <taohu@tencent.com>
2022-02-01 19:02:15 +0800

    feat: add leetcode 76

---
+++ leet_code/c76/c76.cpp
@@ -0,0 +1,62 @@
+//
+// Created by taohu on 2022/2/1.
+//
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> need, window;
+        for (char c: t) {
+            need[c]++;
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        int start = 0, len = INT_MAX;
+        while (right < s.size()) {
+            // c是将移入窗口的字符
+            char c = s[right];
+            // 右移窗口
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.count(c)) {
+                window[c]++;
+                if (window[c] == need[c]) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧窗口是否要收缩
+            while (valid == need.size()) {
+                // 在这里更新最小覆盖子串
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                // d 是将移出窗口的字符
+                char d = s[left];
+                // 左移窗口
+                left++;
+                // 进行窗口内数据的一些列更新
+                if (need.count(d)) {
+                    if (window[d] == need[d]) {
+                        valid--;
+                    }
+                    window[d]--;
+                }
+            }
+        }
+        return len == INT_MAX ? "" : s.substr(start, len);
+    }
+};
+
+int main() {
+    cout << "Hello World" << endl;
+    Solution s;
+    cout << s.minWindow("ADOBECODEBANC", "ABC") << endl;
+    return 0;
+}
